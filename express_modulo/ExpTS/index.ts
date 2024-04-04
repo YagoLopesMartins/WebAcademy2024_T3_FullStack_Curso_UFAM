6ccea7035e802bffb07e373825b783926936e2af
Yago <ylm@icomp.ufam.edu.br>
2024-04-04 01:47:03 -0300

    Exercicio 10 - ExpTS - Organizando projeto para layouts no handlebars

--- express_modulo/ExpTS/index.ts
+++ express_modulo/ExpTS/index.ts
@@ -17,7 +17,9 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", `${__dirname}/views`);
 app.engine("handlebars", engine({
-  helpers: require(`${__dirname}/views/helpers/helpers.ts`)
+  helpers: require(`${__dirname}/views/helpers/helpers.ts`),
+  layoutsDir: `${__dirname}/views/layouts/`
+  // defaultLayout: 'main'
 }));
 
 const PORT = process.env.PORT || 3000;
