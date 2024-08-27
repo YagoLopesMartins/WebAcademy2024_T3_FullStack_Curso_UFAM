029b664d7170578b6e2ae8b8bb55a5c591ab32da
Yago <ylm@icomp.ufam.edu.br>
2024-08-27 03:15:05 -0400

    feat: api rest simples com banco de dados in memory e express

---
+++ modulo09_Basico_EXPRESS/rest_api/index.js
@@ -0,0 +1,104 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+var DB = {
+    games: [
+        {
+            id: 1,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
+        },
+        {
+            id: 2,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
+        },
+        {
+            id: 3,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
+        }
+    ]
+}
+
+app.get('/games', (req, res) => {
+    res.statusCode = 200
+    res.json(DB.games)
+})
+app.get('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    }else{
+        var id = parseInt(req.params.id)
+        var game = DB.games.find(game => game.id == id)
+        if (game != undefined){
+            res.statusCode = 200
+            res.json(game)
+        }else{
+            res.sendStatus(404)
+        }
+    }
+})
+
+app.post('/game', (req, res) => {
+    const {title, year, price} = req.body;
+
+    DB.games.push({
+        id: 4,
+        title,
+        year,
+        price
+    })
+
+    res.sendStatus(200)
+})
+
+app.delete('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    }else{
+        var id = parseInt(req.params.id)
+        var index = DB.games.findIndex(game => game.id == id)
+        if (index == -1){
+            res.statusCode = 404
+        }else{
+            DB.games.splice(index, 1)
+            res.sendStatus(200)
+        }
+    }
+})
+
+app.put('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400)
+    }else{
+        var id = parseInt(req.params.id)
+        var game = DB.games.find(game => game.id == id)
+        if (game != undefined){
+            const {title, year, price} = req.body;
+
+            if(title != undefined) {
+                game.title = title
+            }
+            if(price != undefined) {
+                game.title = title
+            }
+            if(year != undefined) {
+                game.title = title
+            }
+            res.sendStatus(200)
+        }else{
+            res.sendStatus(404)
+        }
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Express app iniciada na porta ${PORT}.`)
+})
