df941ad3b9d162d8212a0efaca2c3730acc475bf
Yago <ylm@icomp.ufam.edu.br>
2024-08-27 18:12:16 -0400

    feat:  Add jwt and routes /auth

--- modulo09_Basico_EXPRESS/rest_api/index.js
+++ modulo09_Basico_EXPRESS/rest_api/index.js
@@ -2,12 +2,37 @@ const express = require("express")
 
 const app = express()
 const  cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const JWT_SECRET = "asfsdfsdfsdfsdfsçdf´ç541s51dfs1df1541"
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 app.use(cors())
+
+function auth(req, res, next) {
+    const authToken = req.headers['authorization']
+    if(authToken != undefined) {
+        const bearer = authToken.split(' ')
+        var token = bearer[1]
+
+        jwt.verify(token, JWT_SECRET, (err, data) => {
+            if(err) {
+                res.status(401)
+                res.json({ err: "Token invalido"})
+            }else{
+                req.token = token
+                req.loggedUser = { id: data.id, email: data.email}
+                next()
+            }
+        })
+    }else{
+        res.status(401)
+        res.json({err: "Token invalido"})
+    }
+}
 
 var DB = {
     games: [
@@ -29,10 +54,24 @@ var DB = {
             year: 2021,
             price: 80
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Yago",
+            email: "yagolopesmartins777@gmail.com",
+            password: "java123"
+        },
+        {
+            id: 2,
+            name: "Pedro",
+            email: "pedro@gmail.com",
+            password: "node123"
+        }
     ]
 }
 
-app.get('/games', (req, res) => {
+app.get('/games', auth, (req, res) => {
     res.statusCode = 200
     res.json(DB.games)
 })
@@ -53,14 +92,12 @@ app.get('/game/:id', (req, res) => {
 
 app.post('/game', (req, res) => {
     const {title, year, price} = req.body;
-
     DB.games.push({
         id: 4,
         title,
         year,
         price
     })
-
     res.sendStatus(200)
 })
 
@@ -87,7 +124,6 @@ app.put('/game/:id', (req, res) => {
         var game = DB.games.find(game => game.id == id)
         if (game != undefined){
             const {title, year, price} = req.body;
-
             if(title != undefined) {
                 game.title = title
             }
@@ -101,6 +137,39 @@ app.put('/game/:id', (req, res) => {
         }else{
             res.sendStatus(404)
         }
+    }
+})
+
+app.post("/auth", (req, res) => {
+    var {email, password} = req.body
+    if(email != undefined){
+        var user = DB.users.find(user => user.email == email)
+        if (user != undefined) {
+            if (user.password == password) {
+                
+                jwt.sign({
+                    id: user.id,
+                    email: user.email
+                }, JWT_SECRET, {expiresIn:'48h'}, (err, token) => {
+                    if(err){
+                        res.status(400)
+                        res.json({ err: "Falha interna"})
+                    }else{
+                        res.status(200)
+                        res.json({ token: token})
+                    }
+                })                
+            }else{
+                res.status(401)
+                res.json({ err: "Crredenciais invalidas"})
+            }
+        }else{
+            res.status(404)
+            res.json({ err: "E-mail não existe"})
+        }
+    }else{
+        res.status(400)
+        res.json({ err: "E-mail inválido"})
     }
 })
 
